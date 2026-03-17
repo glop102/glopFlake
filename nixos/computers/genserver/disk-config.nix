7950b79768abc7c54ed7573bd5fcfdbcdf9b25a3
glop102 <alexlikeschess@gmail.com>
2026-03-17 00:51:07 -0400

    Add the genserver disk config

--- nixos/computers/genserver/disk-config.nix
+++ nixos/computers/genserver/disk-config.nix
@@ -3,34 +3,37 @@
   imports = [
     flakeInputs.disko.nixosModules.disko
   ];
-  config.disko.devices = {
-    disk.main-os = {
-      device = "/dev/nvme0n1";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            name = "ESP";
-            start = "1MiB";
-            size = "1GiB";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+  config = {
+    disko.devices = {
+      disk.main-os = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              name = "ESP";
+              start = "1MiB";
+              size = "1GiB";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
             };
-          };
-          rootfs = {
-            name = "rootfs";
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
+            rootfs = {
+              name = "rootfs";
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
             };
           };
         };
       };
     };
+    boot.zfs.extraPools = [ "RAID" ];
   };
 }
