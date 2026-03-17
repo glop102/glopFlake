261d5e653728a3ea849e95bd3f8eeab181ceaa63
glop102 <alexlikeschess@gmail.com>
2026-03-17 06:48:04 +0000

    Add amdgpu to the initrd

--- nixos/computers/genserver/desktop.nix
+++ nixos/computers/genserver/desktop.nix
@@ -7,8 +7,11 @@
   environment.systemPackages = with pkgs; [
   ];
 
+  # Turn on graphics acceleration
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
   };
+  # Allow the gpu module to be added to the initrd for earlier init
+  boot.initrd.kernelModules = ["amdgpu"];
 }
