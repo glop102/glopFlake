be1b377375e3789281a33dc6cd75f89243e21e83
glop102 <alexlikeschess@gmail.com>
2026-03-24 03:00:10 -0400

    vscode extension for sublime keybinds

--- nixos/modules/desktop/common_utils.nix
+++ nixos/modules/desktop/common_utils.nix
@@ -36,17 +36,27 @@
 
     programs.vscode = {
       enable = true;
-      extensions = with pkgs.vscode-extensions; [
-        # Python things
-        ms-python.python # unclear what this encompasses
-        ms-python.debugpy # debugger
-        ms-python.vscode-pylance # LSP
-        ms-python.mypy-type-checker
-        #ms-python.pylint # linting - highlights every single function if you don't put a docstring
+      extensions =
+        with pkgs.vscode-extensions;
+        [
+          # Python things
+          ms-python.python # unclear what this encompasses
+          ms-python.debugpy # debugger
+          ms-python.vscode-pylance # LSP
+          ms-python.mypy-type-checker
+          #ms-python.pylint # linting - highlights every single function if you don't put a docstring
 
-        # Nix things
-        bbenoist.nix
-      ];
+          # Nix things
+          bbenoist.nix
+        ]
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "sublime-keybindings";
+            publisher = "ms-vscode";
+            version = "4.1.10";
+            sha256 = "sha256-XlogenuBmP+tE18VLH4lUSpOq/7d022n8HgXnKjY3n0=";
+          }
+        ];
     };
 
     programs = {
