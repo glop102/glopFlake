30585ff2514e172578a038104ac2b94569920f16
glop102 <glop102@glop.tech>
2026-08-05 21:48:05 -0400

    Turn on rocm runtime for genserver

--- nixos/computers/genserver/desktop.nix
+++ nixos/computers/genserver/desktop.nix
@@ -7,6 +7,8 @@
   };
   # Allow the gpu module to be added to the initrd for earlier init
   boot.initrd.kernelModules = [ "amdgpu" ];
+  # Build GPU compute packages against ROCm rather than CPU/CUDA
+  nixpkgs.config.rocmSupport = true;
 
   environment.etc."sway/config.d/outputs.conf".text = ''
     output DP-1 position 0,0
